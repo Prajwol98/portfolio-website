b90011588619625c6b7136a7b858126536c8363b
Prajwol Mainali <prajwolmainali96@gmail.com>
2024-01-02 20:08:09 +0545

    complete the website

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,66 @@
+import { useRef } from "react";
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "Forest Research and Training Centre, Kathmandu",
+    Position: "Intern Student",
+    Year: "January 2022 - June 2022",
+  },
+];
+
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.Position}</h2>
+            <p>{item.Year}</p>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Portfolio = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,88 @@
+import { useRef } from "react";
+import "./services.scss";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Services = () => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
+  return (
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      // animate="animate"
+      // whileInView="animate"
+      ref={ref}
+      animate={"animate"}
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <h1>
+            <motion.b whileHover={{ color: "Green" }}>My Education</motion.b>{" "}
+          </h1>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>
+            School Level <br></br>Certificate
+          </h2>
+          <p>
+            Adarsha English Boarding School, Kapilvastu Graduation Year: 2015
+          </p>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Intermediate Level</h2>
+          <p>
+            Hetauda School of Management & Social Science, Hetauda Graduation
+            Year: 2017
+          </p>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Bachelors in Foresty</h2>
+          <p>Faculty of Foresty, Hetauda Graduation Year: 2022</p>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Masters in Foresty</h2>
+          <p>Faculty of Foresty, Hetauda Graduation Year: 2023-Running</p>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
